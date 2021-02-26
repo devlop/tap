306920147cf3cec5475710e5877afbceed753661
Johan <johan@devlop.se>
2021-02-26 15:28:47 +0700

    add tests

--- tests/tap.test.ts
+++ tests/tap.test.ts
@@ -1,11 +1,59 @@
 import tap from '../src/tap';
 
 describe('tap', () => {
-    it('returns the value', () => {
+    it('returns the initial value', () => {
+        let value = 'initial';
 
+        expect(tap(value, function (value) {
+            // do nothing
+        })).toBe('initial');
     });
 
     it('invokes the callback', () => {
+        let value = 'initial';
 
+        let callbackInvoked = false;
+
+        tap(value, function (value) {
+            callbackInvoked = true;
+        })
+
+        expect(callbackInvoked).toBe(true);
+    });
+
+    it('invokes the callback only once', () => {
+        let value = 'initial';
+
+        let callbackInvokedCount = 0;
+
+        tap(value, function (value) {
+            callbackInvokedCount += 1;
+        })
+
+        expect(callbackInvokedCount).toBe(1);
+    });
+
+    it('passes the value as first argument in the callback', () => {
+        let value = 'initial';
+
+        tap(value, function (value) {
+            expect(value).toBe('initial');
+        })
+    });
+
+    it('does not return the callback return value', () => {
+        let value = 'initial';
+
+        expect(tap(value, function (value) {
+            return 'changed';
+        })).not.toBe('changed');
+    });
+
+    it('does not return a changed value', () => {
+        let value = 'initial';
+
+        expect(tap(value, function (value) {
+            value = 'changed';
+        })).not.toBe('changed');
     });
 });
